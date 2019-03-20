1f7cc43582d774f4b8e4cb07f3cff038975482fd
Chofongsua <purinat.san@student.mahidol.ac.th>
2019-03-20 21:49:53 +0700

    Update main function to have user input

--- project.c
+++ project.c
@@ -30,29 +30,27 @@ void initialize(N, M, E, T)
 int main(void)
 {
 
-    int numberOfThread = 5;
-    int numberOfSimulator = 1;
-    int simulatorPeriod = 10;
-    int learningPeriod = 10;
+    int numberOfThread;
+    int numberOfSimulator;
+    int simulatorPeriod;
+    int learningPeriod;
+
+    // int numberOfThread = 5;
+    // int numberOfSimulator = 1;
+    // int simulatorPeriod = 10;
+    // int learningPeriod = 10;
+
+    // For User input
+    printf("Enter the amount of Tachikoma robots :");
+    scanf("%d", &numberOfThread);
+    printf("Enter the amount of Simulator :");
+    scanf("%d", &numberOfSimulator);
+    printf("Enter the maximum period of time that a simulator runs in one playing (in seconds) :");
+    scanf("%d", &simulatorPeriod);
+    printf("Enter the fixed period of time that the whole learning will take :");
+    scanf("%d", &learningPeriod);
 
     initialize(numberOfThread, numberOfSimulator, simulatorPeriod, learningPeriod);
 
     return 0;
 }
-
-// #include <stdio.h>
-// #include <pthread.h>
-
-// void *hello(void *input)
-// {
-//     printf("%s\n", (char *)input);
-//     pthread_exit(NULL);
-// }
-
-// int main(void)
-// {
-//     pthread_t tid;
-//     pthread_create(&tid, NULL, hello, "Test printing");
-//     pthread_join(tid, NULL);
-//     return 0;
-// }
